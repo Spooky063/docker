36ea614ab7462a67681d199851df33b53c2d844e
Guillaume Astier <astier.gui@gmail.com>
2018-01-02 19:06:19 +0100

    Update docker-entrypoint.sh

--- drupal/php-fpm/docker/docker-entrypoint.sh
+++ drupal/php-fpm/docker/docker-entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 set -e
 
-# first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+  set -- php-fpm "$@"
 fi
 
 if [ "$1" = 'php-fpm' ]; then
-  # The first time volumes are mounted, the project needs to be recreated
   if [ ! -f composer.json ]; then
     composer create-project drupal/drupal:$DRUPAL_VERSION /var/www/html
   fi
 
-	chown -R www-data:www-data .
+  chown -R www-data:www-data .
 fi
 
 exec docker-php-entrypoint "$@"
